ee5072ee69c0c9ee214227cb1043a3e7e1a89188
Ana Otero <anajova711@gmail.com>
2023-07-31 11:19:27 -0400

    homepage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,19 @@
 import React from "react";
+import "./Home.css";
 
 export const Home = () => {
   return (
     <section className='home-container' id='home'>
-      <div>This is home page</div>
+      <div>
+        <div id='home-first'>
+          <div>PREMIUM</div>
+          <div>BARTENDING</div>
+          <div>SERVICES</div>
+        </div>
+        <p id='home-second'>
+          Hire a Wedding, Event or Party Bartenders & Wait Staff Today!
+        </p>
+      </div>
     </section>
   );
 };
