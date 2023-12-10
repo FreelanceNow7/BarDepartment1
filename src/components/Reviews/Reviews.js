1404ea5ecabc6aea27f492380c6819c964e44aaf
Ana Otero <anajova711@gmail.com>
2023-12-09 19:49:13 -0500

    text updated

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Reviews.css";
 
 export const Reviews = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const reviews = [
+    "Sarah W.: Absolutely fantastic experience with Bar Department! The Classic Elegance Package added the perfect touch to my wedding. The bartenders were professional, the drinks were exquisite, and the bar setup was stunning. Our guests couldn't stop raving about the service. Thank you for making our special day even more memorable!",
+
+    "Alex M.: Bar Department brought the wow factor to our corporate event with the Signature Mixology Experience. The mixologists were not only skilled but also entertaining. The customized cocktails were a huge hit, and the mobile bar setup was a game-changer. The attention to detail and professionalism were top-notch. Highly recommend!",
+
+    "Mark and Emily: Bar Department made our backyard celebration a breeze with the Casual Celebrations Package. The bartenders were friendly, the setup was perfect for our laid-back vibe, and the flexible beverage menu pleased everyone. Coordinating with the caterers was seamless, and the cleanup afterward was thorough. Will definitely hire them for future events!",
+
+    "Isabella R.: Bar Department elevated our gala with the Luxury Libations Experience. The master mixologists created a unique and sophisticated drink menu that impressed our VIP guests. The top-shelf bar setup and luxurious decor added a touch of glamour. Outstanding service from start to finish. A class act!",
+
+    "Ryan H.: Bar Department is our go-to for our weekly Thirsty Thursdays events. The social bartenders are not only skilled but also create a lively atmosphere. The rotating beverage menus keep things exciting, and the themed bar nights have become a crowd favorite. Their social media engagement adds an extra layer of fun. Highly recommended for regular social gatherings!",
+
+    "Jessica P.: Bar Department made our family celebration unforgettable with the Family-Friendly Fiesta Package. The bartenders were not only professional but also great with the kids. The kid-friendly drink options were a hit, and the all-ages activities kept everyone entertained. The themed decor tied everything together perfectly. Can't thank them enough for making our event a success!",
+  ];
+
+  const nextReview = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % reviews.length);
+  };
+
+  const prevReview = () => {
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + reviews.length) % reviews.length
+    );
+  };
   return (
     <section className='reviews-container' id='reviews'>
       <div className='reviews-container'>
         <div className='reviews-title'>WHAT OUR CLIENTS SAY ABOUT US</div>
-        <div className='reviews-text'></div>
+        <div className='review-item'>
+          <p>{reviews[currentIndex]}</p>
+        </div>
+        <div className='navigation-buttons'>
+          <button onClick={prevReview}>Previous</button>
+          <button onClick={nextReview}>Next</button>
+        </div>
       </div>
     </section>
   );
--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -10,65 +10,58 @@ export const Services = () => {
           <div id='services-text'>
             <div id='first-row'>
               <div id='service1'>
-                <h3>Service 1</h3>
+                <h3>Classic Elegance Package</h3>
                 <p>
-                  With A Twist Bartending Service most popular and inclusive
-                  Bartending Package that will exceed your expectations. Our
-                  Bartenders will bring everything needed for complete beverage
-                  service at your event. This package is perfect for those who
-                  want a hassle-free service for all of your beverage needs.
+                  A sophisticated offering suitable for weddings and formal
+                  events, featuring experienced bartenders, a full bar setup,
+                  and a customized beverage menu with a touch of elegance.
                 </p>
               </div>
               <div id='service2'>
-                <h3>Service 2</h3>
+                <h3>Signature Mixology Experience</h3>
                 <p>
-                  With A Twist Bartending Service most popular and inclusive
-                  Bartending Package that will exceed your expectations. Our
-                  Bartenders will bring everything needed for complete beverage
-                  service at your event. This package is perfect for those who
-                  want a hassle-free service for all of your beverage needs.
+                  Tailored for corporate events and cocktail parties. This
+                  package showcases expert mixologists, a mobile bar setup,
+                  interactive mixology demonstrations, and a customized menu of
+                  signature cocktails.
                 </p>
               </div>
               <div id='service3'>
-                <h3>Service 3</h3>
+                <h3>Casual Celebrations Package</h3>
                 <p>
-                  With A Twist Bartending Service most popular and inclusive
-                  Bartending Package that will exceed your expectations. Our
-                  Bartenders will bring everything needed for complete beverage
-                  service at your event. This package is perfect for those who
-                  want a hassle-free service for all of your beverage needs.
+                  Designed for informal gatherings and outdoor events, this
+                  package includes skilled bartenders, a limited bar setup, a
+                  flexible beverage menu, and a casual bar decor for a relaxed
+                  atmosphere.
                 </p>
               </div>
             </div>
             <div id='second-row'>
               <div id='service4'>
-                <h3>Service 4</h3>
+                <h3>Luxury Libations Experience</h3>
                 <p>
-                  With A Twist Bartending Service most popular and inclusive
-                  Bartending Package that will exceed your expectations. Our
-                  Bartenders will bring everything needed for complete beverage
-                  service at your event. This package is perfect for those who
-                  want a hassle-free service for all of your beverage needs.
+                  An upscale package for luxurious events and galas, offering
+                  master mixologists, a top-shelf bar setup, an exclusive
+                  beverage menu featuring rare selections, and opulent bar decor
+                  with VIP service.
                 </p>
               </div>
               <div id='service5'>
-                <h3>Service 5</h3>
+                <h3>Thirsty Thursdays Social Package</h3>
                 <p>
-                  With A Twist Bartending Service most popular and inclusive
-                  Bartending Package that will exceed your expectations. Our
-                  Bartenders will bring everything needed for complete beverage
-                  service at your event. This package is perfect for those who
-                  want a hassle-free service for all of your beverage needs.
+                  Tailored for social clubs and weekly networking events, this
+                  package features social bartenders, a portable weekly bar
+                  setup, rotating beverage menus, themed bar nights, and
+                  interactive social media engagement.
                 </p>
               </div>
               <div id='service6'>
-                <h3>Service 6</h3>
+                <h3>Family-Friendly Fiesta Package</h3>
                 <p>
-                  With A Twist Bartending Service most popular and inclusive
-                  Bartending Package that will exceed your expectations. Our
-                  Bartenders will bring everything needed for complete beverage
-                  service at your event. This package is perfect for those who
-                  want a hassle-free service for all of your beverage needs.
+                  Ideal for family celebrations and outdoor gatherings, this
+                  package offers friendly bartenders, kid-friendly drink
+                  options, a casual bar setup, all-ages activities, and
+                  coordinated themes for a family-friendly fiesta.
                 </p>
               </div>
             </div>
