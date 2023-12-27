44b72faaaeba2503823e5bdb8d1918d3058c5a99
Ana Otero <anajova711@gmail.com>
2023-12-27 16:41:50 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,45 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Main } from "./components/Main/Main";
 import "./App.css";
 
-function IntroMessage() {
-  return (
-    <div className='intro'>
-      <p>Our Story: Crafting Moments, One Sip At The Time!</p>
-    </div>
-  );
-}
-
 function App() {
-  const [showIntro, setShowIntro] = useState(true);
-  const [showWebsite, setShowWebsite] = useState(false);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowIntro(false);
-      setShowWebsite(true);
-    }, 3000);
-
-    return () => {
-      clearTimeout(timer);
-    };
-  }, []);
-
   return (
     <div className='App'>
-      <div className={`intro ${showIntro ? "" : "fade-out"}`}>
-        {showIntro && <IntroMessage />}
-      </div>
-      <div className={`website ${showWebsite ? "visible" : "hidden"}`}>
-        {showWebsite && (
-          <Router>
-            <Routes>
-              <Route path='/' element={<Main />} />
-            </Routes>
-          </Router>
-        )}
-      </div>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Main />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
