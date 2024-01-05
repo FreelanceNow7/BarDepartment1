5df609274dbc674b1f3ccdcb8f1b01aa454d9421
Anica77 <anajova711@gmail.com>
2024-01-05 13:06:30 -0500

    Ao deployment (#15)
    * comments deleted
    
    * fixing smaller screens view

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -62,37 +62,40 @@ export const Navbar = () => {
   return (
     <nav className={`navbar ${scrolling ? "scroll" : ""}`}>
       <div className='nav-link-container'>
-        <Link
-          className='nav-bar-link'
-          offset={-100}
-          to='home'
-          spy={true}
-          smooth='true'
-          activeClass={activeSection === "home" ? "active" : ""}
-        >
-          HOME
-        </Link>
-        <Link
-          className='nav-bar-link'
-          offset={-100}
-          to='whyus'
-          spy={true}
-          smooth='true'
-          activeClass={activeSection === "whyus" ? "active" : ""}
-        >
-          WHY US
-        </Link>
-        <Link
-          className='nav-bar-link'
-          offset={-100}
-          to='services'
-          spy={true}
-          smooth='true'
-          activeClass={activeSection === "services" ? "active" : ""}
-        >
-          SERVICES
-        </Link>
+        <div className='first'>
+          <Link
+            className='nav-bar-link'
+            offset={-100}
+            to='home'
+            spy={true}
+            smooth='true'
+            activeClass={activeSection === "home" ? "active" : ""}
+          >
+            HOME
+          </Link>
+          <Link
+            className='nav-bar-link'
+            offset={-100}
+            to='whyus'
+            spy={true}
+            smooth='true'
+            activeClass={activeSection === "whyus" ? "active" : ""}
+          >
+            WHY US
+          </Link>
+          <Link
+            className='nav-bar-link'
+            offset={-100}
+            to='services'
+            spy={true}
+            smooth='true'
+            activeClass={activeSection === "services" ? "active" : ""}
+          >
+            SERVICES
+          </Link>
+        </div>
         <img className='logo' src={logo} alt='BarDepLogo'></img>
+        <div className='second'>
         <Link
           className='nav-bar-link'
           offset={-100}
@@ -116,16 +119,38 @@ export const Navbar = () => {
         <button id='getaquotebutton'>
           <Link
             className='nav-bar-link'
-            id='linkgetaquote'
-            to='getAQuote'
+            offset={-100}
+            to='reviews'
             spy={true}
-            smooth={true}
-            offset={-250}
-            activeClass={activeSection === "getAQuote" ? "active" : ""}
+            smooth='true'
+            activeClass={activeSection === "reviews" ? "active" : ""}
           >
-            GET A QUOTE
+            REVIEWS
           </Link>
-        </button>
+          <Link
+            className='nav-bar-link'
+            offset={-100}
+            to='contact'
+            spy={true}
+            smooth='true'
+            activeClass={activeSection === "contact" ? "active" : ""}
+          >
+            CONTACT US
+          </Link>{" "}
+          <button id='getaquotebutton'>
+            <Link
+              className='nav-bar-link'
+              id='linkgetaquote'
+              to='getAQuote'
+              spy={true}
+              smooth={true}
+              offset={-250}
+              activeClass={activeSection === "getAQuote" ? "active" : ""}
+            >
+              GET A QUOTE
+            </Link>
+          </button>
+        </div>
       </div>
     </nav>
   );
