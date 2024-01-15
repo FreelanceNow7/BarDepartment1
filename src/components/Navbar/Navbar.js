e876f4c196df6684b31d585e05f4bb21780af468
Ana Otero <anajova711@gmail.com>
2024-01-15 14:50:12 -0500

    navbar fixed

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -96,27 +96,26 @@ export const Navbar = () => {
         </div>
         <img className='logo' src={logo} alt='BarDepLogo'></img>
         <div className='second'>
-        <Link
-          className='nav-bar-link'
-          offset={-100}
-          to='reviews'
-          spy={true}
-          smooth='true'
-          activeClass={activeSection === "reviews" ? "active" : ""}
-        >
-          REVIEWS
-        </Link>
-        <Link
-          className='nav-bar-link'
-          offset={-100}
-          to='contact'
-          spy={true}
-          smooth='true'
-          activeClass={activeSection === "contact" ? "active" : ""}
-        >
-          CONTACT US
-        </Link>{" "}
-        
+          <Link
+            className='nav-bar-link'
+            offset={-100}
+            to='reviews'
+            spy={true}
+            smooth='true'
+            activeClass={activeSection === "reviews" ? "active" : ""}
+          >
+            REVIEWS
+          </Link>
+          <Link
+            className='nav-bar-link'
+            offset={-100}
+            to='contact'
+            spy={true}
+            smooth='true'
+            activeClass={activeSection === "contact" ? "active" : ""}
+          >
+            CONTACT US
+          </Link>{" "}
           <button id='getaquotebutton'>
             <Link
               className='nav-bar-link'
@@ -130,6 +129,7 @@ export const Navbar = () => {
               GET A QUOTE
             </Link>
           </button>
+        </div>
       </div>
     </nav>
   );
